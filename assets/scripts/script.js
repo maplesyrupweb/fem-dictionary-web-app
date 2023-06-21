89a218b8dec8c825de8d2a12e13fc2f39a0f3a9f
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-06-21 16:41:03 -0700

    Output first result of API to screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,12 @@
 
 let theFormInput = document.getElementById("formInput");
 let theResult = document.getElementById("result");
+let theAudio = document.getElementById("audio");
+let thePhonetic = document.getElementById("phonetic");
+let thePartOfSpeech = document.getElementById("partOfSpeech");
+let theMeaning = document.getElementById("meaning");
+let theSynonym = document.getElementById("synonym");
+let theSource = document.getElementById("source");
 
 function  processForm() {
 
@@ -23,7 +29,29 @@ async function getWord(wordInput)
         if (response.ok) {
             
             console.log("Dictionary API result is: " , result);                
+            
+            
+            console.log("Word: " + result[0].word);
             theResult.innerHTML = result[0].word;    
+
+            console.log("Audio: " + result[0].phonetics[1].audio);
+            theAudio.innerHTML  = result[0].phonetics[1].audio;
+
+            console.log("Phonetic: " + result[0].phonetic);
+            thePhonetic.innerHTML = result[0].phonetic;
+
+            console.log("Part of speech: " + result[0].meanings[0].partOfSpeech);
+            thePartOfSpeech.innerHTML = result[0].meanings[0].partOfSpeech;
+
+            console.log("The meaning: " + result[0].meanings[0].definitions[0].definition ); 
+            theMeaning.innerHTML = result[0].meanings[0].definitions[0].definition;
+            
+            console.log("Synonym: " + result[0].meanings[0].synonyms);
+            theSynonym.innerHTML = result[0].meanings[0].synonyms;
+    
+            console.log("The source: " + result[0].sourceUrls[0]);
+            theSource.innerHTML = result[0].sourceUrls[0];
+            
         }
 
     }  
