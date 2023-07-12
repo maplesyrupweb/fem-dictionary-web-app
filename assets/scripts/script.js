6b224f67ba6d3884a103dca7623cd776e8bb28d3
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-07-11 23:39:33 -0700

    Display all meanings of a word

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,8 +14,24 @@ let hrSource = document.getElementById("hr-source");
 let meaningTitle = document.getElementById("meaningHeading");
 let synonymTitle = document.getElementById("synonymHeading");
 let sourceTitle = document.getElementById("sourceHeading");
-let keywordPlayButton = document.querySelector("[data-keyword__play]");
 
+
+const searchButton = document.getElementById("search-button");
+const searchTerm = document.getElementById("search-term");
+const keywordPlayButton = document.querySelector("[data-keyword__play]");
+
+
+searchButton.addEventListener("click", () => initiateSearch());
+
+//search keyword or hashtag
+function initiateSearch(keyword) {
+  const searchFor = keyword || searchTerm.value;
+  if (searchFor) {
+    location.assign(
+      location.href.split("#")[0] + "#" + encodeURIComponent(searchFor)
+    );
+  }
+}
 
 
 function  processForm() {
@@ -93,7 +109,9 @@ async function getWord(wordInput)
             sourceTitle.classList.add("show");
             console.log("The source: " + result[0].sourceUrls[0]);
             theSource.innerHTML = result[0].sourceUrls[0];
-            
+         
+            console.log("calling fillDefinitions");
+            fillDefinitions(result);
         }
 
     }  
@@ -107,14 +125,14 @@ async function getWord(wordInput)
 function fillKeyword(data) {
 
     console.log("*** Inside the fillkeyword function ***")
-    // data-keyword, data-pronunciation, data-audio
-    // const termEl = document.querySelector("[data-keyword__term]");
-    // termEl.innerText = data.word;
+     
+    const termEl = document.querySelector("[data-keyword__term]");
+    termEl.innerText = data.word;
   
-    // const pronunciationEl = document.querySelector(
-    //   "[data-keyword__pronunciation]"
-    // );
-    // pronunciationEl.innerText = data.phonetic || "";
+    const pronunciationEl = document.querySelector(
+      "[data-keyword__pronunciation]"
+    );
+    pronunciationEl.innerText = data.phonetic || "";
   
   
     // *************************************************************
@@ -129,6 +147,28 @@ function fillKeyword(data) {
   }
 
 
+  function fillKeyword2(data) {
+
+      console.log("*** Inside the fillkeyword2 function ***")
+          // data-keyword, data-pronunciation, data-audio
+      const termEl = document.querySelector("[data-keyword__term]");
+      termEl.innerText = data.word;
+
+      const pronunciationEl = document.querySelector(
+        "[data-keyword__pronunciation]"
+      );
+      pronunciationEl.innerText = data.phonetic || "";
+
+      // return first non-empty audio tag
+      let audioUrl2 = data.phonetics.find((e2) => {
+        return e2.audio;
+      });
+      const audioEl2 = document.querySelector("[data-keyword__audio2]");
+      audioEl2?.setAttribute("src", audioUrl2?.audio);
+
+  }
+
+
 /**
  * Event listener for the play button
  */
@@ -137,6 +177,7 @@ keywordPlayButton.addEventListener("click", (evt) => {
     const audioEl = document.querySelector("[data-keyword__audio]");
     audioEl.play();
 });
+
 
 /**
  * Hide the spinner
@@ -207,3 +248,167 @@ function getFontSize() {
 }
 
 window.onload = getFontSize();getFontFamily();
+
+function fillDefinitions(data) {
+    const definitions = document.querySelector("[data-definitions]");
+    // clear contents if any
+    definitions.innerText = "";
+  
+    for (const result of data) {
+      for (const partOfSpeech of result.meanings) {
+        element("h2")
+          .text(partOfSpeech.partOfSpeech)
+          .class("definition__part-of-speech")
+          .class("heading--2")
+          .addTo(definitions);
+        element("h3")
+          .text("Meaning")
+          .class("definition__meaning-label")
+          .class("heading--3")
+          .addTo(definitions);
+        const ulEl = element("ul")
+          .class("definition__meaning-list")
+          .addTo(definitions);
+  
+        for (const defn of partOfSpeech.definitions) {
+          const liEl = element("li").addTo(ulEl);
+          const defnTextEl = element("p")
+            .text(defn.definition)
+            .class("definition__text")
+            .class("text--1")
+            .addTo(liEl);
+          if (defn.example) {
+            element("p")
+              .text(defn.example)
+              .class("definition__example")
+              .class("text--1")
+              .addTo(liEl);
+          }
+  
+          if (defn.synonyms.length) {
+            element("h4")
+              .text("Synonyms")
+              .class("definition__synonym-label")
+              .addTo(liEl);
+            const defnSynUlEl = element("ul")
+              .class("definition__synonym-list")
+              .addTo(liEl);
+            for (const syn of defn.synonyms) {
+              const liEl = element("li")
+                .class("definition__synonym")
+                .addTo(defnSynUlEl);
+              element("a")
+                .text(syn)
+                .attribute(
+                  "href",
+                  location.origin +
+                    location.pathname +
+                    "#" +
+                    encodeURIComponent(syn)
+                )
+                .addTo(liEl);
+            }
+          }
+          if (defn.antonyms.length) {
+            element("h4")
+              .text("Antonyms")
+              .class("definition__antonym-label")
+              .addTo(liEl);
+            const defnAntUlEl = element("ul")
+              .class("definition__antonym-list")
+              .addTo(liEl);
+            for (const ant of defn.antonyms) {
+              const liEl = element("li").class("definition__antonym").addTo(liEl);
+              element("a")
+                .text(ant)
+                .attribute(
+                  "href",
+                  location.origin +
+                    location.pathname +
+                    "#" +
+                    encodeURIComponent(ant)
+                )
+                .addTo(liEl);
+            }
+          }
+        }
+        if (partOfSpeech.synonyms.length) {
+          const div = element("div").addTo(definitions);
+          element("h3")
+            .text("Synonyms")
+            .class("definition__synonym-label")
+            .class("heading--3")
+            .addTo(div);
+          const synUlEl = element("ul")
+            .class("definition__synonym-list")
+            .addTo(div);
+  
+          for (const syn of partOfSpeech.synonyms) {
+            const liEl = element("li")
+              .class("definition__synonym")
+              .addTo(synUlEl);
+            element("a")
+              .text(syn)
+              .attribute(
+                "href",
+                location.origin +
+                  location.pathname +
+                  "#" +
+                  encodeURIComponent(syn)
+              )
+              .addTo(liEl);
+          }
+        }
+        if (partOfSpeech.antonyms.length) {
+          const div = element("div").addTo(definitions);
+          element("h3")
+            .text("Antonyms")
+            .class("definition__antonym-label")
+            .class("heading--3")
+            .addTo(div);
+          const antUlEl = element("ul")
+            .class("definition__antonym-list")
+            .addTo(div);
+  
+          for (const ant of partOfSpeech.antonyms) {
+            const liEl = element("li")
+              .class("definition__antonym")
+              .addTo(antUlEl);
+            element("a")
+              .text(ant)
+              .attribute(
+                "href",
+                location.origin +
+                  location.pathname +
+                  "#" +
+                  encodeURIComponent(ant)
+              )
+              .addTo(liEl);
+          }
+        }
+      }
+      element("h4")
+        .text("Source")
+        .class("definition__source-label")
+        .addTo(definitions);
+      const defnSourceList = element("ul")
+        .class("definition__source-list")
+        .addTo(definitions);
+      for (const url of result.sourceUrls) {
+        const sourceItem = element("li")
+          .class("definition__source-url")
+          .addTo(defnSourceList);
+        const ahref = element("a")
+          .attribute("href", url)
+          .attribute("target", "_blank")
+          .addTo(sourceItem);
+        element("span").text(url).addTo(ahref);
+        element("img")
+          .class("icon")
+          .class("icon-new-window")
+          .attribute("src", newWindowIcon)
+          .attribute("aria-hidden", "true")
+          .addTo(ahref);
+      }
+    }
+  }
